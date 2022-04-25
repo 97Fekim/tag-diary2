e58c5d4e7f8ba7ab29c6942e7da85f4f6612cd1d
Fekim <81150979+97Fekim@users.noreply.github.com>
2022-04-25 21:59:49 +0900

    Initial commit

---
+++ src/main/java/com/fekim/tagdiary2/TagDiary2Application.java
@@ -0,0 +1,13 @@
+package com.fekim.tagdiary2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TagDiary2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TagDiary2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/fekim/tagdiary2/TagDiary2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fekim.tagdiary2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TagDiary2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
