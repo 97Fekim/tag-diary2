12270f9a0df9a8d52baa49e989daf1815d544b88
Fekim <81150979+97Fekim@users.noreply.github.com>
2022-04-26 21:18:20 +0900

    DOCS: comment test code for deploy

--- src/test/java/com/fekim/tagdiary2/security/MemberTests.java
+++ src/test/java/com/fekim/tagdiary2/security/MemberTests.java
@@ -8,45 +8,45 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-@SpringBootTest
-public class MemberTests {
-
-    @Test
-    public void helloWorld(){
-
-    }
-
-    @Autowired
-    private MemberRepository memberRepository;
-
-    @Autowired
-    private PasswordEncoder passwordEncoder;
-
-    @Test
-    public void addRole(){
-
-        Member member1 = Member.builder()
-                .name("Romeo")
-                .password(passwordEncoder.encode("1234"))
-                .email("Romeo@gmail.com")
-                .fromSocial(false)
-                .build();
-
-        Member member2 = Member.builder()
-                .name("Juliet")
-                .password(passwordEncoder.encode("1234"))
-                .email("Juliet@gmail.com")
-                .fromSocial(false)
-                .build();
-
-        member1.addMemberRole(MemberRole.USER);
-        member2.addMemberRole(MemberRole.USER);
-
-        memberRepository.save(member1);
-        memberRepository.save(member2);
-
-    }
-
+//@SpringBootTest
+//public class MemberTests {
+//
+//    @Test
+//    public void helloWorld(){
+//
+//    }
+//
+//    @Autowired
+//    private MemberRepository memberRepository;
+//
+//    @Autowired
+//    private PasswordEncoder passwordEncoder;
+//
+//    @Test
+//    public void addRole(){
+//
+//        Member member1 = Member.builder()
+//                .name("Romeo")
+//                .password(passwordEncoder.encode("1234"))
+//                .email("Romeo@gmail.com")
+//                .fromSocial(false)
+//                .build();
+//
+//        Member member2 = Member.builder()
+//                .name("Juliet")
+//                .password(passwordEncoder.encode("1234"))
+//                .email("Juliet@gmail.com")
+//                .fromSocial(false)
+//                .build();
+//
+//        member1.addMemberRole(MemberRole.USER);
+//        member2.addMemberRole(MemberRole.USER);
+//
+//        memberRepository.save(member1);
+//        memberRepository.save(member2);
+//
+//    }
+//
 //    @Test
 //    public void testJoin(){
 //        Member member = Member.builder()
@@ -58,5 +58,5 @@ public class MemberTests {
 //
 //        memberRepository.save(member);
 //    }
-
-}
+//
+//}
