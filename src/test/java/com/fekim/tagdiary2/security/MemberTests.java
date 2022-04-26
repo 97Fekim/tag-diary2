dd618be9180d87de2da5e0b81d7e3ff2b30757dd
97Fekim <rlatmdcjf9@gmail.com>
2022-04-26 16:53:19 +0900

    REFACTOR: RDS and MariaDB setting

--- src/test/java/com/fekim/tagdiary2/security/MemberTests.java
+++ src/test/java/com/fekim/tagdiary2/security/MemberTests.java
@@ -1,7 +1,12 @@
 package com.fekim.tagdiary2.security;
 
+import com.fekim.tagdiary2.member.domain.Member;
+import com.fekim.tagdiary2.member.domain.MemberRepository;
+import com.fekim.tagdiary2.member.domain.MemberRole;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootTest
 public class MemberTests {
@@ -11,36 +16,36 @@ public class MemberTests {
 
     }
 
-//    @Autowired
-//    private MemberRepository memberRepository;
-//
-//    @Autowired
-//    private PasswordEncoder passwordEncoder;
-//
-//    @Test
-//    public void addRole(){
-//
-//        Member member1 = Member.builder()
-//                .name("Romeo")
-//                .password(passwordEncoder.encode("1234"))
-//                .email("Romeo@gmail.com")
-//                .fromSocial(false)
-//                .build();
-//
-//        Member member2 = Member.builder()
-//                .name("Juliet")
-//                .password(passwordEncoder.encode("1234"))
-//                .email("Juliet@gmail.com")
-//                .fromSocial(false)
-//                .build();
-//
-//        member1.addMemberRole(MemberRole.USER);
-//        member2.addMemberRole(MemberRole.USER);
-//
-//        memberRepository.save(member1);
-//        memberRepository.save(member2);
-//
-//    }
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Test
+    public void addRole(){
+
+        Member member1 = Member.builder()
+                .name("Romeo")
+                .password(passwordEncoder.encode("1234"))
+                .email("Romeo@gmail.com")
+                .fromSocial(false)
+                .build();
+
+        Member member2 = Member.builder()
+                .name("Juliet")
+                .password(passwordEncoder.encode("1234"))
+                .email("Juliet@gmail.com")
+                .fromSocial(false)
+                .build();
+
+        member1.addMemberRole(MemberRole.USER);
+        member2.addMemberRole(MemberRole.USER);
+
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+    }
 
 //    @Test
 //    public void testJoin(){
