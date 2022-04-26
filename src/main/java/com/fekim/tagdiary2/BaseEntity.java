84152ba0e65cc7c7b37cf0971c440b6d5d181b22
97Fekim <rlatmdcjf9@gmail.com>
2022-04-26 12:51:10 +0900

    TEST: test for deploy

--- src/main/java/com/fekim/tagdiary2/BaseEntity.java
+++ src/main/java/com/fekim/tagdiary2/BaseEntity.java
@@ -15,6 +15,7 @@ import java.time.LocalDateTime;
 @Getter
 public class BaseEntity {
 
+    //
     @CreatedDate
     @Column(name = "regdate", updatable = false)
     private LocalDateTime regDate;
