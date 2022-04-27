7d68277d6678d1f2ec726fb18963c6dd8d033ebd
97Fekim <rlatmdcjf9@gmail.com>
2022-04-27 18:04:52 +0900

    FEAT: Add category('hobby') and Modify Register page

--- src/main/java/com/fekim/tagdiary2/tag/domain/TagRepository.java
+++ src/main/java/com/fekim/tagdiary2/tag/domain/TagRepository.java
@@ -2,6 +2,10 @@ package com.fekim.tagdiary2.tag.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TagRepository extends JpaRepository<Tag, Long> {
+
+    List<Tag> findAllByTagType(String type);
 
 }
