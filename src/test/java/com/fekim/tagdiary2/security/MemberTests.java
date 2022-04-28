545ef4780c48a006cd344906fb1c73d681a01f05
Fekim <81150979+97Fekim@users.noreply.github.com>
2022-04-29 00:15:46 +0900

    Merge

--- src/test/java/com/fekim/tagdiary2/security/MemberTests.java
+++ src/test/java/com/fekim/tagdiary2/security/MemberTests.java
@@ -1,13 +1,13 @@
-package com.fekim.tagdiary2.security;
-
-import com.fekim.tagdiary2.member.domain.Member;
-import com.fekim.tagdiary2.member.domain.MemberRepository;
-import com.fekim.tagdiary2.member.domain.MemberRole;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
+//package com.fekim.tagdiary2.security;
+//
+//import com.fekim.tagdiary2.member.domain.Member;
+//import com.fekim.tagdiary2.member.domain.MemberRepository;
+//import com.fekim.tagdiary2.member.domain.MemberRole;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//
 //@SpringBootTest
 //public class MemberTests {
 //
