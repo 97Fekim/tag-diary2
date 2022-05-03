5ae2f681f3f8a595d2233c81de64b682ed045275
97Fekim <rlatmdcjf9@gmail.com>
2022-05-03 15:59:55 +0900

    TEST: 회원, 비밀번호 테스트코드 추가

--- src/test/java/com/fekim/tagdiary2/security/MemberTests.java
+++ src/test/java/com/fekim/tagdiary2/security/MemberTests.java
@@ -1,62 +1,50 @@
-//package com.fekim.tagdiary2.security;
-//
-//import com.fekim.tagdiary2.diary.domain.Member;
-//import com.fekim.tagdiary2.diary.domain.MemberRepository;
-//import com.fekim.tagdiary2.diary.domain.MemberRole;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//
-//@SpringBootTest
-//public class MemberTests {
-//
-//    @Test
-//    public void helloWorld(){
-//
-//    }
-//
-//    @Autowired
-//    private MemberRepository memberRepository;
-//
-//    @Autowired
-//    private PasswordEncoder passwordEncoder;
-//
-//    @Test
-//    public void addRole(){
-//
-//        Member member1 = Member.builder()
-//                .name("Romeo")
-//                .password(passwordEncoder.encode("1234"))
-//                .email("Romeo@gmail.com")
-//                .fromSocial(false)
-//                .build();
-//
-//        Member member2 = Member.builder()
-//                .name("Juliet")
-//                .password(passwordEncoder.encode("1234"))
-//                .email("Juliet@gmail.com")
-//                .fromSocial(false)
-//                .build();
-//
-//        member1.addMemberRole(MemberRole.USER);
-//        member2.addMemberRole(MemberRole.USER);
-//
-//        memberRepository.save(member1);
-//        memberRepository.save(member2);
-//
-//    }
-//
-//    @Test
-//    public void testJoin(){
-//        Member member = Member.builder()
-//                .name("join_test_member")
-//                .password(passwordEncoder.encode("1234"))
-//                .build();
-//
-//        member.addMemberRole(MemberRole.USER);
-//
-//        memberRepository.save(member);
-//    }
-//
-//}
+package com.fekim.tagdiary2.security;
+
+import com.fekim.tagdiary2.diary.domain.Member;
+import com.fekim.tagdiary2.diary.domain.MemberRepository;
+import com.fekim.tagdiary2.diary.domain.MemberRole;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.annotation.Rollback;
+
+import javax.transaction.Transactional;
+
+@SpringBootTest
+@Transactional
+@Rollback
+public class MemberTests {
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Test
+    public void testJoin(){
+
+        Member member1 = Member.builder()
+                .name("Messi")
+                .password(passwordEncoder.encode("1234"))
+                .email("Messi@gmail.com")
+                .fromSocial(false)
+                .build();
+
+        Member member2 = Member.builder()
+                .name("Ronaldo")
+                .password(passwordEncoder.encode("1234"))
+                .email("Ronaldo@gmail.com")
+                .fromSocial(false)
+                .build();
+
+        member1.addMemberRole(MemberRole.USER);
+        member2.addMemberRole(MemberRole.USER);
+
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+    }
+
+}
--- src/test/java/com/fekim/tagdiary2/security/PasswordTests.java
+++ src/test/java/com/fekim/tagdiary2/security/PasswordTests.java
@@ -1,29 +1,29 @@
-//package com.fekim.tagdiary2.security;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//
-//@SpringBootTest
-//public class PasswordTests {
-//
-//    @Autowired
-//    private PasswordEncoder passwordEncoder;
-//
-//    @Test
-//    public void testEncode(){
-//
-//        String password = "1234";
-//
-//        String enPw = passwordEncoder.encode(password);
-//
-//        System.out.printf("enPw : " + enPw);
-//
-//        boolean matchResult = passwordEncoder.matches(password, enPw);
-//
-//        System.out.printf("matchResult : " + matchResult);
-//
-//    }
-//
-//}
+package com.fekim.tagdiary2.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@SpringBootTest
+public class PasswordTests {
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Test
+    public void testEncode(){
+
+        String password = "1234";
+
+        String enPw = passwordEncoder.encode(password);
+
+        System.out.printf("enPw : " + enPw);
+
+        boolean matchResult = passwordEncoder.matches(password, enPw);
+
+        System.out.printf("matchResult : " + matchResult);
+
+    }
+
+}
