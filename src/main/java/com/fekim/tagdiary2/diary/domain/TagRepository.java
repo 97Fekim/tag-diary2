970aa4ae2a61f971e0c304753c1cf1e04ae122cd
Fekim <81150979+97Fekim@users.noreply.github.com>
2022-05-04 23:34:26 +0900

    FIX: íTagRepository의 쿼리에서 @ㅖParam을 붙이지 않아 발생한 버그 수정

--- src/main/java/com/fekim/tagdiary2/diary/domain/TagRepository.java
+++ src/main/java/com/fekim/tagdiary2/diary/domain/TagRepository.java
@@ -2,6 +2,7 @@ package com.fekim.tagdiary2.diary.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -13,6 +14,6 @@ public interface TagRepository extends JpaRepository<Tag, Long> {
             "on t.tno = wu.tag.tno " +
             "where t.tagType= :type " +
             "group by t.tno")
-    List<long[]> getListOfTnoAndCountOfWriteUpByTagType(String type);
+    List<long[]> getListOfTnoAndCountOfWriteUpByTagType(@Param("type")String type);
 
 }
