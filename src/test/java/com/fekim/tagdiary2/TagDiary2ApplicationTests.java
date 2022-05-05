cbace5724aaae5679cf1d66ecfd972122bc2442a
Fekim <81150979+97Fekim@users.noreply.github.com>
2022-05-05 20:03:47 +0900

    DOCS: No mapping 버그 해결을 위해 ã테스트코드 삭제

--- src/test/java/com/fekim/tagdiary2/TagDiary2ApplicationTests.java
+++ src/test/java/com/fekim/tagdiary2/TagDiary2ApplicationTests.java
@@ -1,9 +1,7 @@
 package com.fekim.tagdiary2;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.mock.env.MockEnvironment;
 
 @SpringBootTest
 class TagDiary2ApplicationTests {
@@ -11,59 +9,5 @@ class TagDiary2ApplicationTests {
     @Test
     void contextLoads() {
     }
-
-//    @Test
-//    public void real_profile_이_조회된다() {
-//        // given
-//        String expectedProfile = "real";
-//        MockEnvironment env = new MockEnvironment();
-//        env.addActiveProfile(expectedProfile);
-//        env.addActiveProfile("oauth");
-//        env.addActiveProfile("real-db");
-//
-//        ProfileController controller = new ProfileController(env);
-//
-//        // when
-//        String profile = controller.profile();
-//
-//        // when
-//        Assertions.assertThat(profile).isEqualTo(expectedProfile);
-//    }
-//
-//    @Test
-//    public void real_profile이_없으면_첫번째가_조회된다(){
-//        // given
-//        String expectedProfile = "ouath";
-//
-//        MockEnvironment env = new MockEnvironment();
-//
-//        env.addActiveProfile(expectedProfile);
-//        env.addActiveProfile("real-db");
-//
-//        ProfileController controller = new ProfileController(env);
-//
-//        // when
-//        String profile = controller.profile();
-//
-//        // then
-//        Assertions.assertThat(profile).isEqualTo(expectedProfile);
-//    }
-//
-//    @Test
-//    public void active_profile이_없으면_default가_조회된다(){
-//
-//        //given
-//        String expectedProfile = "default";
-//        MockEnvironment env = new MockEnvironment();
-//
-//        ProfileController controller = new ProfileController(env);
-//
-//        //when
-//        String profile = controller.profile();
-//
-//        //then
-//        Assertions.assertThat(profile).isEqualTo(expectedProfile);
-//
-//    }
 
 }
