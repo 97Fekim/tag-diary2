eb4f32710db4b00e69303cab0d4298fc2a72fe54
Fekim <81150979+97Fekim@users.noreply.github.com>
2022-05-05 19:40:27 +0900

    DOCS: 버그 íNo mapping ´결을 위해 SecurityConfig 수ì 

--- src/main/java/com/fekim/tagdiary2/security/SecurityConfig.java
+++ src/main/java/com/fekim/tagdiary2/security/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
         http.authorizeRequests()
                 .antMatchers("/diarys/**/*").hasRole("USER")
-                        .antMatchers("/profile").permitAll();
+                        .antMatchers("/profile/**/*").permitAll();
 
 
         http.formLogin().successHandler(loginSuccessHandler()).failureHandler(loginFailureHandler());   // 인가/인증에 문제시 로그인 화면, .loginPage()나 .loginProcessUrl() 을 이용해서 커스텀 로그인 페이지 적용 가능
