b4cc3aed9155165daecbd30241d402b6473143f5
Fekim <81150979+97Fekim@users.noreply.github.com>
2022-05-07 22:58:31 +0900

    TEST: 테스트코드 빌드 에러 수정중

--- src/test/java/com/fekim/tagdiary2/TagDiary2ApplicationTests.java
+++ src/test/java/com/fekim/tagdiary2/TagDiary2ApplicationTests.java
@@ -2,10 +2,12 @@ package com.fekim.tagdiary2;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.test.context.web.WebAppConfiguration;
 
 @WebAppConfiguration
 @SpringBootTest
+@PropertySource("classpath:application.properties")
 class TagDiary2ApplicationTests {
 
     @Test
