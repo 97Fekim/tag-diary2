173e4c47e8935a37c68945067134ed2039121b69
Fekim <81150979+97Fekim@users.noreply.github.com>
2022-05-13 21:46:40 +0900

    REFACTOR: 유지보수를 위한 주석 추가

--- src/main/java/com/fekim/tagdiary2/diary/domain/TagRepository.java
+++ src/main/java/com/fekim/tagdiary2/diary/domain/TagRepository.java
@@ -8,6 +8,9 @@ import java.util.List;
 
 public interface TagRepository extends JpaRepository<Tag, Long> {
 
+    /* 가장 인기 있는 태그를 찾기 위해,
+    * List < (tno : 태그의 갯수) > 를 가져옵니다.
+    * tno와 태그의 갯수 모두 long 타입이기 때문에 반환 타입은 List<long[]>입니다. */
     @Query("select t.tno, count(wu.wno) " +
             "from Tag t " +
             "left join WriteUp wu " +
