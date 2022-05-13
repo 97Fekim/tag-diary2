48f6c565aa01d980d8ac0ae81c02fb9d29422396
Fekim <81150979+97Fekim@users.noreply.github.com>
2022-05-13 21:22:38 +0900

    REFACTOR: 유지보수를 위한 주석 추가

--- src/main/java/com/fekim/tagdiary2/diary/service/TagService.java
+++ src/main/java/com/fekim/tagdiary2/diary/service/TagService.java
@@ -7,8 +7,12 @@ import java.util.List;
 
 public interface TagService {
 
+    /* 홈페이지에서 사용할 "가장 인기있는 태그"를 가져옵니다.
+     * 파라미터로 태그의 키워드(일상, 감정 등)을 받습니다.
+     * */
     TagDTO getMostPopularTag(String tagType);
 
+    /* 등록, 수정 페이지에서 사용할 태그 리스트를 가져옵니다 */
     List<TagDTO> getList();
 
     default TagDTO entityToDTO(Tag tag){
