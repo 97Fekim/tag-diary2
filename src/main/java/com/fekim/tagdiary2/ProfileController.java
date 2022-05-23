945579b2c193a104605040b74ee532c14b17bf13
Fekim <81150979+97Fekim@users.noreply.github.com>
2022-05-23 12:13:26 +0900

    FEFACTOR: ProfileController 주석추가

--- src/main/java/com/fekim/tagdiary2/ProfileController.java
+++ src/main/java/com/fekim/tagdiary2/ProfileController.java
@@ -14,16 +14,22 @@ public class ProfileController {
 
     private final Environment env;
 
+    // 현재 구동중인 애플리케이션의 포트 번호를 제공하는 컨트롤러 입니다.
     @GetMapping("/profile")
     public String profile(){
+        // 적용된 proflie 이름들을 리스트로 저장
         List<String> profiles = Arrays.asList(env.getActiveProfiles());
 
+        /* real : 8080 포트
+        * real1 : 8081 포트
+        * real2 : 8082 포트 */
         List<String> realProfiles = Arrays.asList("real", "real1", "real2");
 
+        // 적용중인 profile이 없다면 "default"를 반환합니다.
         String defaultProfile = profiles.isEmpty() ? "default" : profiles.get(0);
 
         return profiles.stream()
-                .filter(realProfiles::contains)
+                .filter(realProfiles::contains) // real1은 8081포트, real2는 8082포트
                 .findAny()
                 .orElse(defaultProfile);
     }
